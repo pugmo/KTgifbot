3f043039732096a365abfedfe5cd17c2543e8790
pugmo <bradlacore@gmail.com>
2016-05-03 01:18:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,62 @@
 var HTTPS = require('https');
 var request = require('request');
 var botID = process.env.BOT_ID;
+var cool = "Bark Bark :)";
 
+function respond2() 
+{
+  var request = JSON.parse(this.req.chunks[0]),
+      botRegex = /poop/;
+
+  if(request.text && botRegex.test(request.text)) 
+  {
+    this.res.writeHead(200);
+    postMessage2();
+    this.res.end();
+  } 
+  else 
+  {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+
+}
+
+function postMessage2() {
+  var botResponse, options, body, botReq;
+
+  botResponse = cool;
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
   trigger = request.text.substring(0,6);
