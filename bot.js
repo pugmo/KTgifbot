e4922c31e2ac0737bf317f8a6ef8b50f4365eb20
pugmo <bradlacore@gmail.com>
2016-05-03 01:40:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var cool = "Bark Bark :)";
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
   trigger = request.text.substring(0,6),
-  trig2 = request.text.substring(0,9);
+  botRegex = /poop/;
   
   if (trigger == '/fetch' && request.name != 'gifbot') 
   {
@@ -16,15 +16,51 @@ function respond() {
     requestLink(searchTerm);
     this.res.end();
   }
-  if (trig2 == 'Good Girl' && request.name != 'gifbot')
-  {
-    searchTerm = request.text.substr(10);
+   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    requestLink2("Bark Bark :)");
+    postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
     this.res.end();
   }
 }
 
+function postMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse = cool();
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 
 function requestLink(searchTerm) {
   request('http://api.giphy.com/v1/gifs/translate?s=' + searchTerm + '&api_key=dc6zaTOxFJmzC&rating=r', function (error, response, body) {
